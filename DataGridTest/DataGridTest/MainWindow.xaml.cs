36d3ba2ebf7ebf38490518b94f1173bedec57705
KimJongHoss <haberde97@gmail.com>
2024-07-25 12:51:36 +0900

    datagrid 예제 추가

--- DataGridTest/DataGridTest/MainWindow.xaml.cs
+++ DataGridTest/DataGridTest/MainWindow.xaml.cs
@@ -19,6 +19,46 @@ namespace DataGridTest
         public MainWindow()
         {
             InitializeComponent();
+
+            List<Data> datas = new List<Data>();
+            datas.Add(new Data { name = "이지원", id = "210651", major = "컴퓨터공학", grade = 1, etc = "" });
+            datas.Add(new Data { name = "김현호", id = "210846", major = "컴퓨터공학", grade = 1, etc = "" });
+            datas.Add(new Data { name = "강희진", id = "210781", major = "컴퓨터공학", grade = 1, etc = "" });
+            datas.Add(new Data { name = "박서준", id = "210945", major = "컴퓨터공학", grade = 1, etc = "" });
+            datas.Add(new Data { name = "강나연", id = "210347", major = "컴퓨터공학", grade = 1, etc = "" });
+            StudentList.ItemsSource = datas;
+        }
+
+        public class Data
+        {
+            public string name { get; set; }
+            public string id { get; set; }
+            public string major { get; set; }
+            public int grade { get; set; }  
+            public string etc { get; set; }
+
+        }
+
+        private void StudentList_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Data dataRow = (Data)StudentList.DataContext; //if문에서 다 가져와서 안해도됨
+            //if (StudentList.SelectedItem is Data data)
+            //{
+            //    string name = data.name;
+            //    MessageBox.Show(name);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("학생을 먼저 선택해주세요!");
+            //}
+
+            string names = string.Empty;
+
+            foreach (Data data in StudentList.SelectedItems)
+            {
+                names += data.name + "\r\n";
+            }
+            MessageBox.Show($"{names}선택됨.");
         }
     }
 }
