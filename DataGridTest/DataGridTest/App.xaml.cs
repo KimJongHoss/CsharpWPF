6a5a8caad44f920172dde0671e0ebc036be207e0
KimJongHoss <haberde97@gmail.com>
2024-07-25 11:42:57 +0900

    imageButton prac

---
+++ DataGridTest/DataGridTest/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DataGridTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
--- WPFToolTest/WPFToolTest/MainWindow.xaml.cs
+++ WPFToolTest/WPFToolTest/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace WPFToolTest
 {
@@ -20,5 +22,26 @@ namespace WPFToolTest
         {
             InitializeComponent();
         }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "JPEG files (*.jpeg)|*.jpeg|PNG files (*.png)|*.png|JFIF files (*.jfif)|*.jfif|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // 첫 번째 이미지 파일만 표시
+                string selectedFile = openFileDialog.FileNames[0];
+                BitmapImage bitmap = new BitmapImage(new Uri(selectedFile));
+                imgViewer.Source = bitmap;
+                btnOpenFile.Width = bitmap.Width;
+                btnOpenFile.Height = bitmap.Height;
+
+            }
+        }
+
+
+
     }
 }
